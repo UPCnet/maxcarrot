1a604095c39cff80e18024eb3b2f186072418d8a
Carles Bruguera <carlesba@gmail.com>
2014-03-31 14:57:51 +0200

    Put test where it belongs...

---
+++ maxcarrot/tests/test_carrot.py
@@ -0,0 +1,19 @@
+import unittest
+
+from maxcarrot import RabbitServer, RabbitClient
+
+
+class FunctionalTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    # BEGIN TESTS
+
+    def test_test(self):
+        """
+
+        """
+        md = True
+
+        self.assertEqual(md, True)
