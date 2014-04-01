83202f1c1b99d35ea6b268d49863048e493d5cbd
Carles Bruguera <carlesba@gmail.com>
2014-04-01 11:08:28 +0200

    Move test base away, add new tests

--- maxcarrot/tests/__init__.py
+++ maxcarrot/tests/__init__.py
@@ -1,1 +1,56 @@
 # -*- coding: utf-8 -*-
+import unittest
+import requests
+import json
+
+from maxcarrot import RabbitServer, RabbitClient
+
+RABBIT_URL = "localhost:5672"
+TEST_VHOST_URL = '{}/tests'.format(RABBIT_URL)
+RABBIT_MANAGEMENT_URL = "http://localhost:15672/api".format(RABBIT_URL)
+
+
+class RabbitTests(unittest.TestCase):
+
+    def setUp(self):
+        self.cleanup()
+        self.server = RabbitServer(TEST_VHOST_URL, 'guest', 'guest', bind=False)
+        self.clients = {}
+
+    def tearDown(self):
+        self.server.disconnect()
+        for user_clients in self.clients.values():
+            for client in user_clients:
+                client.disconnect()
+
+    def cleanup(self):
+        # Delete all exchanges except rabbitmq default ones
+        req = requests.get('{}/exchanges/tests'.format(RABBIT_MANAGEMENT_URL), auth=('guest', 'guest'))
+        for exchange in req.json():
+            if not(exchange['name'] and exchange['name'].startswith('amq.')):
+                requests.delete(
+                    '{}/exchanges/tests/{}'.format(RABBIT_MANAGEMENT_URL, exchange['name']),
+                    data=json.dumps({'vhost': 'tests', 'name': exchange['name']}),
+                    auth=('guest', 'guest')
+                )
+
+        # Delete all exchanges except dynamic ones
+        req = requests.get('{}/queues/tests'.format(RABBIT_MANAGEMENT_URL), auth=('guest', 'guest'))
+        for queue in req.json():
+
+            if not queue['name'].startswith('amq.gen'):
+
+                requests.delete(
+                    '{}/queues/tests/{}'.format(RABBIT_MANAGEMENT_URL, queue['name']),
+                    data=json.dumps({'mode': 'delete', 'vhost': 'tests', 'name': queue['name']}),
+                    auth=('guest', 'guest')
+                )
+
+    def getClient(self, username, reuse=True):
+        self.clients.setdefault(username, [])
+        if not self.clients[username] or not reuse:
+            self.clients[username].append(RabbitClient(TEST_VHOST_URL, username, ''))
+        if reuse:
+            return self.clients[username][0]
+        else:
+            return self.clients[username][-1]
--- maxcarrot/tests/test_carrot.py
+++ maxcarrot/tests/test_carrot.py
@@ -1,53 +1,7 @@
-import unittest
-import requests
-import json
-
-from maxcarrot import RabbitServer, RabbitClient
-
-RABBIT_URL = "localhost:5672"
-TEST_VHOST_URL = '{}/tests'.format(RABBIT_URL)
-RABBIT_MANAGEMENT_URL = "http://localhost:15672/api".format(RABBIT_URL)
+from maxcarrot.tests import RabbitTests
 
 
-class FunctionalTests(unittest.TestCase):
-
-    def setUp(self):
-        self.cleanup()
-        self.server = RabbitServer(TEST_VHOST_URL, 'guest', 'guest', bind=False)
-        self.clients = {}
-
-    def tearDown(self):
-        self.server.disconnect()
-        for client in self.clients.values():
-            client.disconnect()
-
-    def cleanup(self):
-        # Delete all exchanges except rabbitmq default ones
-        req = requests.get('{}/exchanges/tests'.format(RABBIT_MANAGEMENT_URL), auth=('guest', 'guest'))
-        for exchange in req.json():
-            if not(exchange['name'] and exchange['name'].startswith('amq.')):
-                requests.delete(
-                    '{}/exchanges/tests/{}'.format(RABBIT_MANAGEMENT_URL, exchange['name']),
-                    data=json.dumps({'vhost': 'tests', 'name': exchange['name']}),
-                    auth=('guest', 'guest')
-                )
-
-        # Delete all exchanges except dynamic ones
-        req = requests.get('{}/queues/tests'.format(RABBIT_MANAGEMENT_URL), auth=('guest', 'guest'))
-        for queue in req.json():
-
-            if not queue['name'].startswith('amq.gen'):
-
-                requests.delete(
-                    '{}/queues/tests/{}'.format(RABBIT_MANAGEMENT_URL, queue['name']),
-                    data=json.dumps({'mode': 'delete', 'vhost': 'tests', 'name': queue['name']}),
-                    auth=('guest', 'guest')
-                )
-
-    def getClient(self, username):
-        client = self.clients.setdefault(username, RabbitClient(TEST_VHOST_URL, username, ''))
-        return client
-
+class FunctionalTests(RabbitTests):
     # BEGIN TESTS
 
     def test_basic_send_receive(self):
@@ -70,3 +24,93 @@ class FunctionalTests(unittest.TestCase):
 
         self.assertEqual(len(messages_to_sheldon), 2)
         self.assertEqual(len(messages_to_leonard), 2)
+
+    def test_basic_send_receive_all(self):
+        """
+        Given two users in a conversation
+        And one of the users has more than one consumer
+        When each of them writes a message
+        Then all client consumers receives both messages
+        """
+
+        self.server.conversations.create('conversation1', users=['sheldon', 'leonard'])
+
+        sheldon = self.getClient('sheldon')
+        leonard = self.getClient('leonard')
+        leonard2 = self.getClient('leonard', reuse=False)
+
+        sheldon.conversations.send('conversation1', 'Hello!')
+        leonard.conversations.send('conversation1', 'Hello...')
+
+        messages_to_sheldon = sheldon.conversations.get_all('conversation1')
+        messages_to_leonard = leonard.conversations.get_all('conversation1')
+        messages_to_leonard2 = leonard2.conversations.get_all('conversation1')
+
+        self.assertEqual(len(messages_to_sheldon), 2)
+        self.assertEqual(len(messages_to_leonard), 2)
+        self.assertEqual(len(messages_to_leonard2), 2)
+
+    def test_messages_queue_receive(self):
+        """
+        Given a conversation
+        When a message is posted
+        Then the messages queue receives the message
+        And the user also receives the message
+        """
+
+        self.server.conversations.create('conversation1', users=['sheldon', 'leonard'])
+
+        sheldon = self.getClient('sheldon')
+        sheldon.conversations.send('conversation1', 'Hello!')
+        messages_to_sheldon = sheldon.conversations.get_all('conversation1')
+        messages_to_queue = self.server.get_all('messages')
+
+        self.assertEqual(len(messages_to_sheldon), 1)
+        self.assertEqual(len(messages_to_queue), 1)
+
+    def test_messages_queue_receive_all(self):
+        """
+        Given two conversation
+        When a message is posted in each conversation
+        Then the messages queue receives all the messages
+        """
+
+        self.server.conversations.create('conversation1', users=['sheldon', 'leonard'])
+        self.server.conversations.create('conversation2', users=['penny', 'howard'])
+
+        sheldon = self.getClient('sheldon')
+        penny = self.getClient('penny')
+
+        sheldon.conversations.send('conversation1', 'Hello!')
+        penny.conversations.send('conversation2', 'Hello!')
+
+        messages_to_sheldon = sheldon.conversations.get_all('conversation1')
+        messages_to_penny = penny.conversations.get_all('conversation2')
+        messages_to_queue = self.server.get_all('messages')
+
+        self.assertEqual(len(messages_to_sheldon), 1)
+        self.assertEqual(len(messages_to_penny), 1)
+        self.assertEqual(len(messages_to_queue), 2)
+
+    def test_drop_messages_to_unbind_conversation(self):
+        """
+        Given two conversations
+        When a message is posted in a conversation the user isn't binded
+        Then the messages don't get anywhere
+        """
+
+        self.server.conversations.create('conversation1', users=['sheldon', 'leonard'])
+        self.server.conversations.create('conversation2', users=['penny', 'howard'])
+
+        sheldon = self.getClient('sheldon')
+        penny = self.getClient('penny')
+
+        sheldon.conversations.send('conversation2', 'Hello!')
+
+        messages_to_sheldon = sheldon.conversations.get_all('conversation2')
+        messages_to_penny = penny.conversations.get_all('conversation2')
+        messages_to_queue = self.server.get_all('messages')
+
+        self.assertEqual(len(messages_to_sheldon), 0)
+        self.assertEqual(len(messages_to_penny), 0)
+        self.assertEqual(len(messages_to_queue), 0)
